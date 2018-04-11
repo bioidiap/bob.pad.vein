61b8d79f83b6959d1f949206141af6cbe07cc636
Andre Anjos <andre.dos.anjos@gmail.com>
2018-04-11 13:47:12 +0200

    Fix tests

--- bob/pad/vein/test_databases.py
+++ bob/pad/vein/test_databases.py
@@ -2,20 +2,20 @@
 # vim: set fileencoding=utf-8 :
 # Thu May 24 10:41:42 CEST 2012
 
-from nose.plugins.skip import SkipTest
 
+import nose.tools
 import bob.pad.base
 from bob.bio.base.test.utils import db_available
-from bob.bio.base.test.test_database_implementations import check_database
 
 
 @db_available('verafinger')
 def test_verafinger():
-    module = bob.bio.base.load_resource('verafinger', 'config',
-        preferred_package='bob.pad.vein')
-    try:
-        check_database(module.database, protocol='full', groups=('dev',
-          'eval'))
-    except IOError as e:
-        raise SkipTest(
-            "The database could not queried; probably the db.sql3 file is missing. Here is the error: '%s'" % e)
+  module = bob.bio.base.load_resource('verafinger', 'config',
+      preferred_package='bob.pad.vein')
+  try:
+    # counts have already been tested on the low-level database
+    # here, we just check we can access it through the PadDatabase interface
+    groups = ('train', 'dev', 'eval')
+    nose.tools.eq_(len(module.database.objects(groups=groups)), 1760)
+  except IOError as e:
+    raise SkipTest("The database could not queried; probably the db.sql3 file is missing. Here is the error: '%s'" % e)
